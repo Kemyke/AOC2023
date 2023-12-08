08f46b0b83bbad84b6c7e6eff4ed88ef1cc5490f
Attila Kemeny <kemyyy@gmail.com>
2023-12-08 19:42:33 +0100

    Day 8

---
+++ Day08/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day08
+{
+    public class Helper
+    {
+        static int gcf(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        static int lcm(int a, int b)
+        {
+            return (a / gcf(a, b)) * b;
+        }
+    }
+}
---
+++ Day08/Program.cs
@@ -0,0 +1,58 @@
+﻿using MathNet.Numerics;
+using System.ComponentModel.Design;
+
+var input = File.ReadAllLines("input.txt");
+var steps = input.First();
+
+Dictionary<string, (string, string)> network = new Dictionary<string, (string, string)>();
+foreach (var inst in input.Skip(2))
+{
+    var sp = inst.Split(" = ");
+    var cn = sp[0];
+    var sp2 = sp[1].Split(", ");
+    var left = sp2[0].Trim('(');
+    var right = sp2[1].Trim(')');
+
+    network.Add(cn, (left, right));
+}
+
+long ret1 = 0;
+long stepNum = 0;
+var currentNode = "AAA";
+
+List<string> currentNodes = network.Keys.Where(k => k[2] == 'A').ToList();
+Dictionary<int, long> firstZs = new Dictionary<int, long>();
+
+for(int i = 0; i < int.MaxValue; i++)
+{
+    if (steps[i % steps.Length] == 'L')
+    {
+        //currentNode = network[currentNode].Item1;
+        currentNodes = currentNodes.Select(cn => network[cn].Item1).ToList();
+    }
+    else
+    {
+        //currentNode = network[currentNode].Item2;
+        currentNodes = currentNodes.Select(cn => network[cn].Item2).ToList();
+    }
+    //ret1++;
+    stepNum++;
+    //if(currentNode == "ZZZ")
+    //{
+    //    break;
+    //}
+
+    foreach(var cn in currentNodes.Where(c => c[2] == 'Z'))
+    {
+        var idx = currentNodes.IndexOf(cn);
+        if(!firstZs.ContainsKey(idx))
+        {
+            firstZs.Add(idx, stepNum);
+        }
+    }
+
+    if (firstZs.Count == 6)
+        break;
+}
+var ret2 = Euclid.LeastCommonMultiple(firstZs.Values.ToArray());
+Console.ReadLine();
