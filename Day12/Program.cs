d568767b3b7ecb8261a3a5b3c1fa2293f12fc6bc
NEXOGEN\kemenya <kemeny.attila@nexogen.hu>
2023-12-12 12:02:14 +0100

    Day12: part2 broaf code

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -2,74 +2,145 @@
 long ret1 = 0;
 
 var c = 0;
+long ret2 = 0;
 foreach (var line in input)
 {
     var sp = line.Split(" ");
     var damagedStr = sp[1].Split(",");
-    var damaged = damagedStr.Select(d => int.Parse(d)).ToList();
+    var damageds = damagedStr.Select(d => int.Parse(d)).ToList();
     var layout = sp[0] + "?" + sp[0] + "?" + sp[0] + "?" + sp[0] + "?" + sp[0];
-    damaged = damaged.Concat(damaged).Concat(damaged).Concat(damaged).Concat(damaged).ToList();
+    damageds = damageds.Concat(damageds).Concat(damageds).Concat(damageds).Concat(damageds).ToList();
 
-    //Console.WriteLine(sp[1]);
-    //Console.WriteLine(sp[0]);
-    Solve(layout, 0, 0, damaged, "");
-    c++;
-    Console.WriteLine(c);
-    //Console.WriteLine();
-    //Console.WriteLine();
-    //Console.WriteLine();
-    //Console.WriteLine();
+    long sr = 0;
+    long ss = 0;
+    var ca = new Dictionary<string, Dictionary<int, Dictionary<int, long>>>();
+    Solve(layout, 0, 0, damageds, ref ss, ref sr, ca, "");
+    ret2 += sr;
 }
 
-//7619 too high
 
 Console.ReadLine();
 
-void Solve(string layout, int pos, int currentGroupSize, List<int> damageds, string debug)
+void Solve(string layout, int pos, int currentGroupSize, List<int> damageds, ref long subSeqNum, ref long result, Dictionary<string, Dictionary<int, Dictionary<int, long>>> cache, string debug)
 {
-    while (pos < layout.Length && layout[pos] != '?')
+    var ok = layout.Substring(pos);
+    if (cache.ContainsKey(ok) && cache[ok].ContainsKey(damageds.Count) && cache[ok][damageds.Count].ContainsKey(currentGroupSize))
     {
-        if (layout[pos] == '#')
-        {
-            currentGroupSize += 1;
-        }
-        else if (layout[pos] == '.')
-        {
-            if (currentGroupSize == 0)
-            {
-
-            }
-            else if (damageds.Any() && damageds.First() == currentGroupSize)
-            {
-                currentGroupSize = 0;
-                damageds = damageds.Skip(1).ToList();
-            }
-            else
-            {
-                return;
-            }
-        }
-        debug += layout[pos];
-        pos++;
+        subSeqNum += cache[ok][damageds.Count][currentGroupSize];
+        result += cache[ok][damageds.Count][currentGroupSize];
+        return;
     }
 
     if (pos == layout.Length)
     {
-        if ((!damageds.Any() && currentGroupSize == 0) || (damageds.Count == 1 &&currentGroupSize == damageds.Single()))
+        if ((!damageds.Any() && currentGroupSize == 0) || (damageds.Count == 1 && currentGroupSize == damageds.Single()))
         {
             ret1 += 1;
-            //Console.WriteLine(debug);
+            subSeqNum += 1;
+            result += 1;
         }
         return;
     }
 
-    if (damageds.Any() && currentGroupSize + 1 <= damageds.First())
+    if (layout[pos] == '#')
     {
-        Solve(layout, pos + 1, currentGroupSize + 1, damageds, debug + "#");
+        long ss = 0;
+        var k = layout.Substring(pos + 1);
+        if (!cache.ContainsKey(k))
+            cache.Add(k, new Dictionary<int, Dictionary<int, long>>());
+        if (!cache[k].ContainsKey(damageds.Count))
+        {
+            cache[k].Add(damageds.Count, new Dictionary<int, long>());
+        }
+
+        Solve(layout, pos + 1, currentGroupSize + 1, damageds, ref ss, ref result, cache, debug + layout[pos]);
+        subSeqNum += ss;
+        if (!cache[k][damageds.Count].ContainsKey(currentGroupSize + 1))
+        {
+            cache[k][damageds.Count].Add(currentGroupSize + 1, ss);
+        }
     }
-    
-    if (currentGroupSize == 0 || (damageds.Any() && currentGroupSize == damageds.First()))
+    else if (layout[pos] == '.')
     {
-        Solve(layout, pos + 1, 0, (currentGroupSize == 0) ? damageds : damageds.Skip(1).ToList(), debug + ".");
+        if (currentGroupSize == 0)
+        {
+            long ss = 0;
+            var k = layout.Substring(pos + 1);
+            if (!cache.ContainsKey(k))
+                cache.Add(k, new Dictionary<int, Dictionary<int, long>>());
+            if (!cache[k].ContainsKey(damageds.Count))
+            {
+                cache[k].Add(damageds.Count, new Dictionary<int, long>());
+            }
+            Solve(layout, pos + 1, 0, damageds, ref ss, ref result, cache, debug + layout[pos]);
+            subSeqNum += ss;
+            if (!cache[k][damageds.Count].ContainsKey(0))
+            {
+                cache[k][damageds.Count].Add(0, ss);
+            }
+        }
+        else if (damageds.Any() && damageds.First() == currentGroupSize)
+        {
+            long ss = 0;
+            var k = layout.Substring(pos + 1);
+            if (!cache.ContainsKey(k))
+                cache.Add(k, new Dictionary<int, Dictionary<int, long>>());
+            if (!cache[k].ContainsKey(damageds.Count - 1))
+            {
+                cache[k].Add(damageds.Count - 1, new Dictionary<int, long>());
+            }
+
+            Solve(layout, pos + 1, 0, damageds.Skip(1).ToList(), ref ss, ref result, cache, debug + layout[pos]);
+            subSeqNum += ss;
+            if (!cache[k][damageds.Count - 1].ContainsKey(0))
+            {
+                cache[k][damageds.Count - 1].Add(0, ss);
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+    else
+    {
+        if (damageds.Any() && currentGroupSize + 1 <= damageds.First())
+        {
+            long ss = 0;
+            var k = layout.Substring(pos + 1);
+            if (!cache.ContainsKey(k))
+                cache.Add(k, new Dictionary<int, Dictionary<int, long>>());
+            if (!cache[k].ContainsKey(damageds.Count))
+            {
+                cache[k].Add(damageds.Count, new Dictionary<int, long>());
+            }
+
+            Solve(layout, pos + 1, currentGroupSize + 1, damageds, ref ss, ref result, cache, debug + "#");
+            subSeqNum += ss;
+            if (!cache[k][damageds.Count].ContainsKey(currentGroupSize + 1))
+            {
+                cache[k][damageds.Count].Add(currentGroupSize + 1, ss);
+            }
+        }
+
+        if (currentGroupSize == 0 || (damageds.Any() && currentGroupSize == damageds.First()))
+        {
+            long ss = 0;
+            var xd = (currentGroupSize == 0) ? 0 : 1;
+            var k = layout.Substring(pos + 1);
+            if (!cache.ContainsKey(k))
+                cache.Add(k, new Dictionary<int, Dictionary<int, long>>());
+            if (!cache[k].ContainsKey(damageds.Count - xd))
+            {
+                cache[k].Add(damageds.Count - xd, new Dictionary<int, long>());
+            }
+
+            Solve(layout, pos + 1, 0, (currentGroupSize == 0) ? damageds : damageds.Skip(1).ToList(), ref ss, ref result, cache, debug + ".");
+            subSeqNum += ss;
+            if (!cache[k][damageds.Count - xd].ContainsKey(currentGroupSize))
+            {
+                cache[k][damageds.Count - xd].Add(currentGroupSize, ss);
+            }
+        }
     }
 }
