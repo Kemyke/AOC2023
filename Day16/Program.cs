29a57003ac20c1d11725a6bd8faa5f7f74b7ce0a
Attila Kemeny <kemyyy@gmail.com>
2023-12-17 19:05:28 +0100

    Day 16 and 17

---
+++ Day16/Program.cs
@@ -0,0 +1,130 @@
+﻿using AOCHelper;
+using System.Net.Http.Headers;
+
+var input = File.ReadAllLines("input.txt").ToList();
+
+var ret2 = 0;
+for (int i = 0; i < input[0].Length; i++)
+{
+    var map = Helper.ParseInput(input);
+    List<Beam> beams = new List<Beam>();
+    beams.Add(new Beam { CurrentPos = new Item { Coordinate = new Coordinate { X = -1, Y = i } }, LastDir = "E", PathStr = "" });
+
+    while (beams.Any())
+    {
+        var beam = beams.First();
+        var nextPos = new Coordinate { X = beam.CurrentPos.Coordinate.X, Y = beam.CurrentPos.Coordinate.Y };
+        switch (beam.LastDir)
+        {
+            case "E":
+                nextPos.X = nextPos.X + 1;
+                break;
+            case "W":
+                nextPos.X = nextPos.X - 1;
+                break;
+            case "N":
+                nextPos.Y = nextPos.Y - 1;
+                break;
+            case "S":
+                nextPos.Y = nextPos.Y + 1;
+                break;
+        }
+
+        if (map.ContainsKey(nextPos.Y) && map[nextPos.Y].ContainsKey(nextPos.X))
+        {
+            var ci = map[nextPos.Y][nextPos.X];
+
+            if (ci.Lights.ContainsKey(beam.LastDir))
+            {
+                beams = beams.Skip(1).ToList();
+                continue;
+            }
+
+            ci.Energized = true;
+            ci.Lights[beam.LastDir] = 1;
+
+            if (ci.ValueCh != '.')
+            {
+                switch (beam.LastDir)
+                {
+                    case "E":
+                        switch (ci.ValueCh)
+                        {
+                            case '/':
+                                beam.LastDir = "N";
+                                break;
+                            case '\\':
+                                beam.LastDir = "S";
+                                break;
+                            case '|':
+                                beams.Add(new Beam { CurrentPos = ci, LastDir = "N" });
+                                beams.Add(new Beam { CurrentPos = ci, LastDir = "S" });
+                                beams = beams.Skip(1).ToList();
+
+                                break;
+                        }
+                        break;
+                    case "W":
+                        switch (ci.ValueCh)
+                        {
+                            case '/':
+                                beam.LastDir = "S";
+                                break;
+                            case '\\':
+                                beam.LastDir = "N";
+                                break;
+                            case '|':
+                                beams.Add(new Beam { CurrentPos = ci, LastDir = "N" });
+                                beams.Add(new Beam { CurrentPos = ci, LastDir = "S" });
+                                beams = beams.Skip(1).ToList();
+
+                                break;
+                        }
+                        break;
+                    case "N":
+                        switch (ci.ValueCh)
+                        {
+                            case '/':
+                                beam.LastDir = "E";
+                                break;
+                            case '\\':
+                                beam.LastDir = "W";
+                                break;
+                            case '-':
+                                beams.Add(new Beam { CurrentPos = ci, LastDir = "W" });
+                                beams.Add(new Beam { CurrentPos = ci, LastDir = "E" });
+                                beams = beams.Skip(1).ToList();
+                                break;
+                        }
+                        break;
+                    case "S":
+                        switch (ci.ValueCh)
+                        {
+                            case '/':
+                                beam.LastDir = "W";
+                                break;
+                            case '\\':
+                                beam.LastDir = "E";
+                                break;
+                            case '-':
+                                beams.Add(new Beam { CurrentPos = ci, LastDir = "W" });
+                                beams.Add(new Beam { CurrentPos = ci, LastDir = "E" });
+                                beams = beams.Skip(1).ToList();
+                                break;
+                        }
+                        break;
+                }
+            }
+            beam.CurrentPos = ci;
+        }
+        else
+        {
+            beams = beams.Skip(1).ToList();
+        }
+    }
+    var sr = map.SelectMany(k => k.Value).Where(k => k.Value.Energized).Count();
+    if (sr > ret2)
+        ret2 = sr;
+    //var ret1 = map.SelectMany(k => k.Value).Where(k => k.Value.Energized).Count();
+}
+Console.ReadLine();
---
+++ Day17/Program.cs
@@ -0,0 +1,43 @@
+﻿using AOCHelper;
+
+var input = File.ReadAllLines("input.txt").ToList();
+var map = Helper.ParseInput(input);
+List<AOCPath> possiblePathes = new List<AOCPath>();
+Dictionary<long, Dictionary<long, int>> cache = new Dictionary<long, Dictionary<long, int>>();
+
+AOCPath cp = new AOCPath { CurrentPos = map[0][0], HeatLoss = 0, Path = new List<Item> { map[0][0] }, DirCount = 0, LastDir = "" };
+possiblePathes.Add(cp);
+while(cp.CurrentPos.Coordinate.X != map[0].Count - 1 || cp.CurrentPos.Coordinate.Y != map.Count - 1)
+{
+    Step(map, cp);
+    possiblePathes = possiblePathes.Skip(1).OrderBy(k=>k.HeatLoss).ToList();
+    cp = possiblePathes.First();
+}
+Console.ReadLine();
+
+void Step(Dictionary<long, Dictionary<long, Item>> map, AOCPath f)
+{
+    var possItems = Helper.PossibleItems(map, f);
+
+    foreach (var pi in possItems)
+    {
+        var np = new AOCPath
+        {
+            HeatLoss = f.HeatLoss + pi.Item2.Value,
+            CurrentPos = pi.Item2,
+            Path = f.Path.Concat(new List<Item> { pi.Item2 }).ToList(),
+            DirCount = (pi.Item1 == f.LastDir) ? f.DirCount + 1 : 1,
+            LastDir = pi.Item1,
+            PathStr = f.PathStr + pi.Item1,
+        };
+
+        if (!pi.Item2.MinHeatLosses.ContainsKey(pi.Item1) || (!pi.Item2.MinHeatLosses[pi.Item1].ContainsKey(np.DirCount)) ||
+              pi.Item2.MinHeatLosses[pi.Item1][np.DirCount] > np.HeatLoss)
+        {
+            if (!pi.Item2.MinHeatLosses.ContainsKey(pi.Item1))
+                pi.Item2.MinHeatLosses.Add(pi.Item1, new Dictionary<int, int>());
+            pi.Item2.MinHeatLosses[pi.Item1][np.DirCount] = np.HeatLoss;
+            possiblePathes.Add(np);
+        }
+    }
+}
